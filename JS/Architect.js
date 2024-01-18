4ed15f3eb21eb4197dda15a552a96e4dec03d3e2
ST10033851 <ST10033851@vcconnect.edu.za>
2024-01-18 11:49:33 +0200

    Initial Commit

---
+++ JS/Architect.js
@@ -0,0 +1,75 @@
+const cursor = document.querySelector('.cursor');
+const hover = document.querySelectorAll('.Box img');
+const back = document.querySelector('.back');
+const Row = document.querySelector('.Row');
+const menu = document.querySelector('.menu-btn');
+const Gallery = document.querySelector('.Gallery h3');
+
+//mouse mapping and cursor growth on click
+document.addEventListener('mousemove', (e) =>{
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+
+    if(e.target.tagName === 'IMG')
+    {
+        cursor.classList.add('grow');
+    }
+    else{
+        cursor.classList.remove('grow');
+    }
+})
+
+back.addEventListener('mouseover', function() {
+  cursor.classList.add('shrink2');
+});
+
+back.addEventListener('mouseout', function() {
+  cursor.classList.remove('shrink2');
+});
+
+//back button + displaying the grid by section 
+back.addEventListener('click', () =>{
+
+    Row.style.display = 'flex';
+    menu.style.display = 'flex';
+    document.getElementById("portraits").style.display = "none";
+    document.getElementById("Environment").style.display = "none";
+    document.getElementById("Geometry").style.display = "none";
+    
+    // Show the grid element
+    document.getElementById("grid").style.display = "block";
+    
+    setTimeout(() =>{
+        document.getElementById("portraits").style.display = "block";
+      }, 300);
+
+      setTimeout(() =>{
+        document.getElementById("Environment").style.display = "block";
+      }, 400);
+
+      setTimeout(() =>{
+        document.getElementById("Geometry").style.display = "block";
+      }, 500);
+
+      //navigate to the home page 
+    setTimeout(() =>{
+        const url = '/pages/index.html';
+        window.location.href =url;   
+    }, 600)
+})
+
+Gallery.style.fontSize = "60px";
+
+const hoverText = document.querySelector('.Bubble');
+const letters = hoverText.textContent.split('');
+
+hoverText.innerHTML = '';
+letters.forEach(letter => {
+  const span = document.createElement('span');
+  span.textContent = letter;
+  hoverText.appendChild(span);
+});
+
+
+
+
---
+++ JS/script.js
@@ -0,0 +1,158 @@
+const menuBtn = document.querySelector('.menu-btn');
+const cursor = document.querySelector('.cursor');
+const sideBar = document.querySelector('.side-bar');
+const btn = document.querySelector('.button-top');
+const grid = document.querySelector('#grid');
+const overlay = document.querySelector('#overlay');
+const overlayE = document.querySelector('#overlayE');
+const overlayP = document.querySelector('#overlayP');
+const overlayW = document.querySelector('#overlayW');
+const overlayG = document.querySelector('#overlayG');
+const overlayWP = document.querySelector('#overlayWP');
+const Environ = document.querySelector('.small:nth-of-type(1)'); 
+const Portait = document.querySelector('.small:nth-of-type(2)'); 
+const Wierd = document.querySelector('.grid:nth-of-type(3)'); 
+const Geometry = document.querySelector('.grid:nth-of-type(2)'); 
+const Weapon = document.querySelector('.small:nth-of-type(3)'); 
+const Gallery = document.querySelector('.Gallery'); 
+
+let menuOpen = false;
+let cursorShrink = false;
+
+//menu button to open sidebar
+menuBtn.addEventListener('click', () =>{
+    if(!menuOpen)
+    {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    }
+    else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+    
+}),
+
+//cursor mapping
+ document.addEventListener('mousemove', (e) => {
+     cursor.style.left = e.clientX +'px';
+     cursor.style.top = e.clientY +'px';
+ })
+
+ //sidebar text animation
+const menuItems = [...document.querySelectorAll('.menu-item')];
+
+menuItems.forEach(item => {
+  let word = item.children[0].innerText.split('');
+  item.children[0].innerHTML = '';
+  word.forEach((letter, idx) => {
+    item.children[0].innerHTML += `<span style="--index: ${idx}; font-family:'MyCustomFont'">${letter}</span>`;
+  })
+  let cloneDiv = item.children[0].cloneNode(true);
+  cloneDiv.style.position = 'absolute';
+  cloneDiv.style.left= '0';
+  cloneDiv.style.top= '0';
+  item.appendChild(cloneDiv);
+})
+
+menuBtn.addEventListener('click', function() {
+  sideBar.classList.toggle('active');
+}),
+
+//scroll to top of page button
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 300) {
+    btn.classList.add('show');
+  } else {
+    btn.classList.remove('show');
+  }
+});
+
+btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}),
+
+//cursor shink when clicked 
+menuBtn.addEventListener('click', function() {
+
+  cursor.classList.toggle('shrink');
+}),
+
+menuBtn.addEventListener('mouseover', function() {
+  cursor.classList.add('shrink2');
+});
+
+menuBtn.addEventListener('mouseout', function() {
+  cursor.classList.remove('shrink2');
+});
+
+
+grid.addEventListener('click', () => {
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+
+    setTimeout(() => {
+    const url ='../Pages/Architecture.html';
+    window.location.href =url;
+    }, 2000);
+  
+}),
+
+Environ.addEventListener('click', () => {
+  overlayE.style.width = '100%';
+  overlayE.style.height = '100%';
+
+    setTimeout(() => {
+    const url ='../Pages/Environment.html';
+    window.location.href =url;
+    }, 500);
+  
+}),
+
+Portait.addEventListener('click', () => {
+  overlayP.style.width = '100%';
+  overlayP.style.height = '100%';
+
+    setTimeout(() => {
+    const url ='../Pages/Portraits.html';
+    window.location.href =url;
+    }, 500);
+  
+}),
+
+Geometry.addEventListener('click', () => {
+  overlayG.style.width = '100%';
+  overlayG.style.height = '100%';
+
+    setTimeout(() => {
+    const url ='../Pages/GeometryN.html';
+    window.location.href =url;
+    }, 500);
+  
+}),
+
+Weapon.addEventListener('click', () => {
+  overlayWP.style.width = '100%';
+  overlayWP.style.height = '100%';
+
+    setTimeout(() => {
+    const url ='../Pages/Weapons.html';
+    window.location.href =url;
+    }, 500);
+  
+}),
+
+Wierd.addEventListener('click', () => {
+  overlayW.style.width = '100%';
+  overlayW.style.height = '100%';
+
+    setTimeout(() => {
+    const url ='../Pages/Wierd Stuff.html';
+    window.location.href =url;
+    }, 500);
+  
+})
+
+
+
